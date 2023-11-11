b9613f708b1cad5fa0afbcc535a0f4bbd3e22b29
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-11-11 14:39:30 +0700

    Error Page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -10,6 +10,14 @@ const { id } = useRoute().params
 const uri = `https://fakestoreapi.com/products/${id}`
 const { data: product } = await useFetch(uri, { key: id})
 
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    message: 'Product Not Found',
+    fatal: true
+  })
+}
+
 definePageMeta({
   layout:'products'
 })
