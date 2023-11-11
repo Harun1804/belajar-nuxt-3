1d3f0f9a1eab58031489be1d2ff0970674eb0436
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-11-11 09:22:32 +0700

    route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Product Detail for {{ id }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nostrum id molestiae dignissimos corrupti pariatur nam voluptates. Quaerat sapiente dolorem veniam ratione a sit commodi, magnam explicabo non culpa officia consequuntur dolorum perspiciatis, in, amet quia nam ipsam. Doloribus.</p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
