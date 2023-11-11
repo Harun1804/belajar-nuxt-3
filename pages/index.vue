990c54df80fdc1a7eeb63a8ca43eb1ece67b754b
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-11-11 09:38:53 +0700

    Routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
+    <header>
+    <NuxtLink to="/">Ninja Dojo</NuxtLink>
+      <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+      <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </header>
     <h1>Home</h1>
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <header>
+    <NuxtLink to="/">Ninja Dojo</NuxtLink>
+      <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+      <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </header>
     <h1>Products</h1>
   </div>
 </template>
